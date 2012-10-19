0d09594afd15451649374402601c8588a6d046f2
Ohbayashi Ippei <ohai@kmc.gr.jp>
2012-10-20 01:20:02 +0900

    Random#standard_normal: Use Ziggurat method

--- lib/randomext.rb
+++ lib/randomext.rb
@@ -4,6 +4,7 @@ class Random
   #
   # Computed using Polar method.
   def standard_normal
+    raise
     if @z
       r = @z; @z = nil; return r
     end
@@ -52,5 +53,60 @@ class Random
     y1 = standard_normal()
     begin; y2 = standard_normal(); end until y2 != 0.0
     return y1/y2
+  end
+
+  module ZigguratStandardNormal
+    module_function
+    def f(x); Math.exp(-x**2/2); end
+    
+    R = 3.442619855899
+    V = 9.91256303526217e-3
+    K = 7
+    M = 64
+    N = 2**K
+    
+    W = Array.new(N)
+    KK = Array.new(N)
+    F = Array.new(N+1)
+    X = Array.new(N)
+    
+    W[N-1] = V*Math.exp(R**2/2)/(2**(M-K-1))
+    W[N-2] = R/(2**(M-K-1))
+    KK[N-1] = (R/W[N-1]).ceil
+    F[N-1] = Math.exp(-R**2/2)
+    X[N] = V*f(R)
+    X[N-1] = R
+    (N-2).downto(1) do |i|
+      X[i] = Math.sqrt(-2*Math.log(f(X[i+1])+V/X[i+1]))
+      W[i-1] = X[i]/(2**(M-K-1))
+      KK[i] = (X[i]/W[i]).ceil
+      F[i] = Math.exp(-X[i]**2/2)
+    end
+    KK[0] = 0
+    F[0] = 1
+    
+    def generate(rng)
+      loop do 
+        sign = (rng.rand(2) == 0) ? 1 : -1
+        i = rng.rand(N)
+        u = rng.rand(2**(M-K-1))
+        return sign*u*W[i] if u < KK[i]
+        return sign*sample_from_tail(rng) if i == N-1
+        ux = u * W[i]
+        f = Math.exp(-ux**2/2)
+        return sign*ux if rng.rand*(F[i] - F[i+1]) <= f - F[i+1]
+      end
+    end
+
+    def sample_from_tail(rng)
+      loop do
+        x = Math.sqrt(R**2-2*Math.log(1-rng.rand))
+        return x if x*rng.rand <= R
+      end
+    end
+  end
+
+  def standard_normal
+    ZigguratStandardNormal.generate(self)
   end
 end
